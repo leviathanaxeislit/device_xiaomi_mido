crdroidandroid/android_device_xiaomi_mido

mido: Import and adapt keydisabler from LOS tree

BoardConfig.mk
@@ -162,11 +162,12 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 TARGET_INIT_VENDOR_LIB := libinit_msm8953
 TARGET_RECOVERY_DEVICE_MODULES := libinit_msm8953
 
-# Lights
-TARGET_PROVIDES_LIBLIGHT := true
-
 # Lineage Hardware
 JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
+
+# Key disabler
+JAVA_SOURCE_OVERLAYS := \
+    org.pixelexperience.keydisabler|$(DEVICE_PATH)/keydisabler|**/*.java
 
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true

leviathanaxeislit/device_xiaomi_mido

BoardConfig.mk
@@ -167,6 +167,10 @@ TARGET_PROVIDES_LIBLIGHT := true
 # Lineage Hardware
 JAVA_SOURCE_OVERLAYS := org.lineageos.hardware|$(DEVICE_PATH)/lineagehw|**/*.java
 
+# Key disabler
+JAVA_SOURCE_OVERLAYS := \
+    org.pixelexperience.keydisabler|$(VENDOR_PATH)/keydisabler|**/*.java
+
 # Media
 TARGET_USES_MEDIA_EXTENSIONS := true
 