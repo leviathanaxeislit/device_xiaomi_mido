c8cf14404d6cd8ab109747a19e0c22268c0bc479
Aryan <arykedare@gmail.com>
2019-03-02 15:57:16 +0530

    mido: PixelExperience CAF Bringup

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -22,8 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
-
+$(call inherit-product, vendor/aosp/config/common.mk)
 TARGET_GAPPS_ARCH := arm64
 export CUSTOM_BUILD_TYPE=OFFICIAL
 
