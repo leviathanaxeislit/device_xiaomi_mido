6ae7dd8e80b59cf8fe65fd9b5a3e866c4356a73c
ZeeLog <Danjel.Zammit@gmail.com>
2019-05-13 13:13:52 +0200

    mido: cleanup GPS properties
    * mostly unused/useless, besides, support for
      QCOM location has been previously dropped.
    
    Change-Id: I74cadf1d9ae57baa0a596401332b5ab5feb00f8b

--- prop.mk
+++ prop.mk
@@ -129,12 +129,6 @@ vendor.hw.fm.init=0
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.frp.pst=/dev/block/bootdevice/by-name/config
 
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
-
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 media.msm8956hw=0 \
